0c621b5545194176e8cf1ac005c37ff261202b1d
kuhuhalder <kuhuhalder2701@gmail.com>
2023-02-25 17:43:52 -0500

    update

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -3,12 +3,14 @@ const app = express.Router();
 
 const Movie = require('../../models/Movies');
 
-// // @route GET api/movies
-// // @description tests movies route
-// // @access Public
-// app.get('/test', (req, res) => {
-//   res.send("Movie route is running")
-// })
+// @route POST api/movies
+// @description add/save movie
+// @access Public
+app.post('/', (req, res) => {
+  Movie.create(req.body)
+    .then(movie => res.json({ msg: 'Movie added successfully' }))
+    .catch(err => res.status(400).json({ error: 'Unable to add this movie' }));
+})
 
 // @route GET api/movies
 // @description Get all movies
@@ -20,14 +22,6 @@ app.get('/', (req, res) => {
 })
 
 
-// @route POST api/movies
-// @description add/save movie
-// @access Public
-app.post('/', (req, res) => {
-  Movie.create(req.body)
-    .then(movie => res.json({ msg: 'Movie added successfully' }))
-    .catch(err => res.status(400).json({ error: 'Unable to add this movie' }));
-})
 
 
 module.exports = app;
